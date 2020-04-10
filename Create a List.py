b9b371b56196ad7b3c28b56bcdbefb99804a25bb
shivam7374 <shivamsingla491@gmail.com>
2020-04-10 23:15:41 +0530

    Creating a List

---
+++ Create a List.py
@@ -0,0 +1,4 @@
+  # LIST OF AUTO PATRS
+List = [["BRAKE SYSTEM", "RS.350"] , ["EXHAUST FILTER","RS.320"], ["CAR ACCESSORIES","RS.300"], ["AIR CONDITIONING","RS.500"]] 
+for i in range(0,4):
+    print(List[i])
