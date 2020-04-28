44e528acff39fb2103a8d116b9e1cdc116f53936
shivam7374 <shivamsingla491@gmail.com>
2020-04-28 22:50:10 +0530

    13

---
+++ 13.py
@@ -0,0 +1,14 @@
+#13
+#printing R
+print("*"*4)
+i = 1
+while(i<=2):
+    print("*"+" "*2+"*")
+    i+=1
+print("*"*4)
+j = 1
+while(j<4):
+    print(end = "*")
+    print(" "*j,end="")
+    print("*")
+    j+=1
