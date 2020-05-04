b8ea871456edc5adeae68fd44ce236fed0e33a76
shivam7374 <shivamsingla491@gmail.com>
2020-05-04 13:07:53 +0530

    --

--- 4.sorttupfloat.py
+++
@@ -1,7 +0,0 @@
-def Sort(tup):
-
-    return (sorted(tup, key=lambda x: float(x[1]), reverse=True))
-
-
-tup = [('item1', '12.20'), ('item2', '15.10'), ('item3', '24.5')]
-print(Sort(tup))
--- 7.count.py
+++
@@ -1,7 +0,0 @@
-num = [10,20,30,(10,20),40]
-ctr = 0
-for n in num:
-    if isinstance(n, tuple):
-        break
-    ctr += 1
-print(ctr)
