cfcd8c6f6cfc0881d67bca6f5afaa419e0ebd548
shivam7374 <shivamsingla491@gmail.com>
2020-05-04 13:25:54 +0530

    --

--- 23.py
+++
@@ -1,6 +0,0 @@
-import uuid
-
-
-uuidOne = uuid.uuid1().hex[:8]
-print ("random string using a UUID module is")
-print(uuidOne)
--- 6.py
+++
@@ -1,9 +0,0 @@
-def Convert(tup, di):
-    di = dict(tup)
-    return di
-
-
-# Driver Code
-tups = ((2, "w"),(3, "r"))
-dictionary = {}
-print(Convert(tups, dictionary))
